d6a4d1f66a6f9eaee1d3632b04a9277cb1287a6c
cmtevilela <tereza.cabocla@gmail.com>
2016-08-03 14:52:22 -0300

    ruby_basics/hashes exercises included

---
+++ exercises/ruby_basics/hashes/1.rb
@@ -0,0 +1,7 @@
+# Create a hash that contains the following data and assign it to a 
+# variable named car.
+
+# type  color mileage
+# sedan blue  80_000
+
+car = { type: "sedan", color: "blue", mileage: 80_000 }
---
+++ exercises/ruby_basics/hashes/7.rb
@@ -0,0 +1,17 @@
+# Use Hash#select to iterate over numbers and return a hash containing only 
+# key-value pairs where the value is less than 25. Assign the returned array 
+# to a variable named low_numbers and print its value using #p.
+
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
+
+# Expected output:
+
+# {low: 10}
+
+low_numbers = numbers.select { |k,v| v < 25 }
+
+p low_numbers
